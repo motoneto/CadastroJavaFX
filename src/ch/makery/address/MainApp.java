f82102bb6fc2a1a84bfc6163065e0757740d0407
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-24 15:34:07 -0300

    Pasta images criada e icone definido no metodo start

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.collections.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -69,6 +70,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        this.primaryStage.getIcons().add(new Image("file:resources/images/icone.png"));
 
         initRootLayout();
 
